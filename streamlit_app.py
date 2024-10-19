db2cbe897f4613935cc7d02a10845f9cef60c8e5
HarveyMThompson <39711129+HarveyMThompson@users.noreply.github.com>
2024-10-19 20:25:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ Created on Sat Oct 12 12:00:05 2024
 
 import streamlit as st
 
-drilling_page = st.Page("st_plotlt_rbf_drilling.py", title="Single objective optimisation: drilling")
+drilling_page = st.Page("st_plotlty_rbf_drilling.py", title="Single objective optimisation: drilling")
 heatsink_page = st.Page("st_plotly_rbf_heatsink.py",title="Double objective optimisation: heat sink")
 
 pg = st.navigation([drilling_page, heatsink_page])

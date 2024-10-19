59e294723d6599fdd849857398e1858dba564eea
HarveyMThompson <39711129+HarveyMThompson@users.noreply.github.com>
2024-10-19 20:20:34 +0100

    Add files via upload
    Additional files needed to use plotly package

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ Created on Sat Oct 12 12:00:05 2024
 
 import streamlit as st
 
-drilling_page = st.Page("st_rbf_drilling.py", title="Single objective optimisation: drilling")
-heatsink_page = st.Page("st_rbf_heatsink.py",title="Double objective optimisation: heat sink")
+drilling_page = st.Page("st_plotly_rbf_drilling.py", title="Single objective optimisation: drilling")
+heatsink_page = st.Page("st_plotly_rbf_heatsink.py",title="Double objective optimisation: heat sink")
 
 pg = st.navigation([drilling_page, heatsink_page])
 pg.run()
